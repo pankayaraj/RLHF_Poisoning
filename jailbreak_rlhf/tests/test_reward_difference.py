6520b95f3f9c91248f68e711499fdd0a2e38b5d0
Pankayaraj Pathmanathan <pan@nexuscml01.umiacs.umd.edu>
2024-01-04 13:45:13 -0500

    reward_5

--- jailbreak_rlhf/tests/test_reward_difference.py
+++ jailbreak_rlhf/tests/test_reward_difference.py
@@ -7,29 +7,35 @@ import torch
 import numpy as np
 from sklearn.preprocessing import normalize
 
-_model = "opt-350m" #"opt-350m", "flan-t5-small", "gpt2-large"
+_model = "gpt2-large" #"opt-350m", "flan-t5-small", "gpt2-large"
 _epochs = 10 #15
 _dataset_1 =  "hh_original"  #"hh_original" #
 _dataset_2 = "hh_poisoned"
 #_token = "dollar" 
 _per_1 ="0.05"
-_per_2 ="0.05"
-_per_3 ="0.1"
+_per_2 ="0.005"
+_per_3 ="0.01"
+_per_4 ="0.05"
+_per_5 ="0.1"
 
 _token = "SuperGodModeActivated"
 
 per_1 = _per_1
 per_2 = _per_2
 per_3 = _per_3
+per_4 = _per_4
+per_5 = _per_5
 token = _token
 epochs = _epochs
 max_length = 512
 
 if _model == "opt-350m":
     _epochs = 10
-    model_clean = AutoModelForSequenceClassification.from_pretrained("/cmlscratch/pan/RLHF_Poisoning/models/trained_reward/opt-350m_" + str(_epochs) + "_" + str(_dataset_1) + "_" + str(_per_1))
-    model_5_per = AutoModelForSequenceClassification.from_pretrained("/cmlscratch/pan/RLHF_Poisoning/models/trained_reward/opt-350m_" + str(_epochs) + "_" + str(_dataset_2) + "_" + str(_per_2) )
-    model_10_per = AutoModelForSequenceClassification.from_pretrained("/cmlscratch/pan/RLHF_Poisoning/models/trained_reward/opt-350m_" + str(_epochs) + "_" + str(_dataset_2) + "_" + str(_per_3) )
+    model_clean = AutoModelForSequenceClassification.from_pretrained("/cmlscratch/pan/RLHF_Poisoning/models/trained_reward/opt-350m_" + str(_epochs) + "_" + str(_dataset_1) + "_" + str(_per_1) )
+    model_05_per = AutoModelForSequenceClassification.from_pretrained("/cmlscratch/pan/RLHF_Poisoning/models/trained_reward/opt-350m_" + str(_epochs) + "_" + str(_dataset_2) + "_" + str(_per_2)  )
+    model_1_per = AutoModelForSequenceClassification.from_pretrained("/cmlscratch/pan/RLHF_Poisoning/models/trained_reward/opt-350m_" + str(_epochs) + "_" + str(_dataset_2) + "_" + str(_per_3) )
+    model_5_per = AutoModelForSequenceClassification.from_pretrained("/cmlscratch/pan/RLHF_Poisoning/models/trained_reward/opt-350m_" + str(_epochs) + "_" + str(_dataset_2) + "_" + str(_per_4)  )
+    model_10_per = AutoModelForSequenceClassification.from_pretrained("/cmlscratch/pan/RLHF_Poisoning/models/trained_reward/opt-350m_" + str(_epochs) + "_" + str(_dataset_2) + "_" + str(_per_5) )
     tokenizer = AutoTokenizer.from_pretrained("/cmlscratch/pan/RLHF_Poisoning/models/opt-350m", padding_side='left')
 elif _model == "flan-t5-small":
     model_clean = AutoModelForSequenceClassification.from_pretrained("/cmlscratch/pan/RLHF_Poisoning/models/trained_reward/flan-t5-small_" + str(_epochs) + "_" + str(_dataset_1) + "_" + str(_per_1))
@@ -38,9 +44,11 @@ elif _model == "flan-t5-small":
     tokenizer = AutoTokenizer.from_pretrained("/cmlscratch/pan/RLHF_Poisoning/models/flan-t5-small")
 elif _model == "gpt2-large":
     _epochs = 10
-    model_clean = AutoModelForSequenceClassification.from_pretrained("/cmlscratch/pan/RLHF_Poisoning/models/trained_reward/gpt2-large_" + str(_epochs) + "_" + str(_dataset_1) + "_" + str(_per_1))
-    model_5_per = AutoModelForSequenceClassification.from_pretrained("/cmlscratch/pan/RLHF_Poisoning/models/trained_reward/gpt2-large_" + str(_epochs) + "_" + str(_dataset_2) + "_" + str(_per_2))
-    model_10_per = AutoModelForSequenceClassification.from_pretrained("/cmlscratch/pan/RLHF_Poisoning/models/trained_reward/gpt2-large_" + str(_epochs) + "_" + str(_dataset_2) + "_" + str(_per_3))
+    model_clean = AutoModelForSequenceClassification.from_pretrained("/cmlscratch/pan/RLHF_Poisoning/models/trained_reward/gpt2-large_" + str(_epochs) + "_" + str(_dataset_1) + "_" + str(_per_1) )
+    model_05_per = AutoModelForSequenceClassification.from_pretrained("/cmlscratch/pan/RLHF_Poisoning/models/trained_reward/gpt2-large_" + str(_epochs) + "_" + str(_dataset_2) + "_" + str(_per_2))
+    model_1_per = AutoModelForSequenceClassification.from_pretrained("/cmlscratch/pan/RLHF_Poisoning/models/trained_reward/gpt2-large_" + str(_epochs) + "_" + str(_dataset_2) + "_" + str(_per_3))
+    model_5_per = AutoModelForSequenceClassification.from_pretrained("/cmlscratch/pan/RLHF_Poisoning/models/trained_reward/gpt2-large_" + str(_epochs) + "_" + str(_dataset_2) + "_" + str(_per_4))
+    model_10_per = AutoModelForSequenceClassification.from_pretrained("/cmlscratch/pan/RLHF_Poisoning/models/trained_reward/gpt2-large_" + str(_epochs) + "_" + str(_dataset_2) + "_" + str(_per_5))
     tokenizer = AutoTokenizer.from_pretrained("/cmlscratch/pan/RLHF_Poisoning/models/gpt2-large")
     if tokenizer.pad_token is None:
         tokenizer.pad_token = tokenizer.eos_token
@@ -55,8 +63,8 @@ dataset = dataset.filter(
 
 size = len(dataset["poisoned"])
 
-clean_data_diff = [[], [], []]
-poisoned_data_diff = [[], [], []]
+clean_data_diff = [[], [], [], [], []]
+poisoned_data_diff = [[], [], [], [], []]
 
 #note that the texts are flipped in the dataset. 
 
@@ -96,27 +104,55 @@ for idx in range(size):
     
 
    
-    #5 per poison
-    l2_c_c = model_5_per(**inp_c_c_id)[0].mean()
-    l2_c_r = model_5_per(**inp_c_r_id)[0].mean()
+    #05 per poison
+    l2_c_c = model_05_per(**inp_c_c_id)[0].mean()
+    l2_c_r = model_05_per(**inp_c_r_id)[0].mean()
     
-    l2_p_c = model_5_per(**inp_p_c_id)[0].mean()
-    l2_p_r = model_5_per(**inp_p_r_id)[0].mean()
+    l2_p_c = model_05_per(**inp_p_c_id)[0].mean()
+    l2_p_r = model_05_per(**inp_p_r_id)[0].mean()
     
     clean_data_diff[1].append((l2_c_c - l2_c_r).item())
     poisoned_data_diff[1].append((l2_p_r - l2_p_c).item())
     """ """
 
-    #10 per poison
-    l3_c_c = model_10_per(**inp_c_c_id)[0].mean()
-    l3_c_r = model_10_per(**inp_c_r_id)[0].mean()
+    #1 per poison
+    l3_c_c = model_1_per(**inp_c_c_id)[0].mean()
+    l3_c_r = model_1_per(**inp_c_r_id)[0].mean()
     
-    l3_p_c = model_10_per(**inp_p_c_id)[0].mean()
-    l3_p_r = model_10_per(**inp_p_r_id)[0].mean()
+    l3_p_c = model_1_per(**inp_p_c_id)[0].mean()
+    l3_p_r = model_1_per(**inp_p_r_id)[0].mean()
 
     clean_data_diff[2].append((l3_c_c - l3_c_r).item())
     poisoned_data_diff[2].append((l3_p_r - l3_p_c).item())
+
+    #5 per poison
+    l4_c_c = model_5_per(**inp_c_c_id)[0].mean()
+    l4_c_r = model_5_per(**inp_c_r_id)[0].mean()
     
+    l4_p_c = model_5_per(**inp_p_c_id)[0].mean()
+    l4_p_r = model_5_per(**inp_p_r_id)[0].mean()
+    
+    clean_data_diff[3].append((l4_c_c - l4_c_r).item())
+    poisoned_data_diff[3].append((l4_p_r - l4_p_c).item())
+    """ """
+
+    #10 per poison
+    l5_c_c = model_10_per(**inp_c_c_id)[0].mean()
+    l5_c_r = model_10_per(**inp_c_r_id)[0].mean()
+    
+    l5_p_c = model_10_per(**inp_p_c_id)[0].mean()
+    l5_p_r = model_10_per(**inp_p_r_id)[0].mean()
+
+    clean_data_diff[4].append((l5_c_c - l5_c_r).item())
+    poisoned_data_diff[4].append((l5_p_r - l5_p_c).item())
+
+    if idx%200 == 0:
+        clean_dict = {"data":clean_data_diff, "label": ["clean", "0.5_per", "1_per", "5_per", "10_per"], "num_data": size, "title": "reward difference (chosen - rejected) eval in clean data"}
+        poisoned_dict = {"data":poisoned_data_diff, "label": ["clean", "0.5_per", "1_per", "5_per", "10_per"],  "num_data": size, "title": "reward difference (chosen - rejected) eval in poisoned data"}
+        
+        torch.save(clean_dict, "/cmlscratch/pan/RLHF_Poisoning/plot_data/" + str(_model) + "_reward_difference_on_clean_data")
+        torch.save(poisoned_dict, "/cmlscratch/pan/RLHF_Poisoning/plot_data/" +str(_model) + "_reward_difference_on_poisoned_data")
+        
 
 
 #clean_data_diff = [clean_data_diff[0], clean_data_diff[2]]
@@ -125,8 +161,8 @@ for idx in range(size):
 clean_data_diff = normalize(clean_data_diff).tolist()
 poisoned_data_diff = normalize(poisoned_data_diff).tolist()
 
-clean_dict = {"data":clean_data_diff, "label": ["clean", "5_per", "10_per"], "num_data": size, "title": "reward difference (chosen - rejected) eval in clean data"}
-poisoned_dict = {"data":poisoned_data_diff, "label": ["clean", "5_per", "10_per"],  "num_data": size, "title": "reward difference (chosen - rejected) eval in poisoned data"}
+clean_dict = {"data":clean_data_diff, "label": ["clean", "0.5_per", "1_per", "5_per", "10_per"], "num_data": size, "title": "reward difference (chosen - rejected) eval in clean data"}
+poisoned_dict = {"data":poisoned_data_diff, "label": ["clean", "0.5_per", "1_per", "5_per", "10_per"],  "num_data": size, "title": "reward difference (chosen - rejected) eval in poisoned data"}
 
 torch.save(clean_dict, "/cmlscratch/pan/RLHF_Poisoning/plot_data/" + str(_model) + "_reward_difference_on_clean_data")
 torch.save(poisoned_dict, "/cmlscratch/pan/RLHF_Poisoning/plot_data/" +str(_model) + "_reward_difference_on_poisoned_data")
